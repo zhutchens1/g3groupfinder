b9636dda9b7540f3caf3041682ecf056c75103e7
Zack Hutchens <zhutchens1@gmail.com>
2022-08-17 22:13:37 -0400

    adding figures folder; updates to make eco group cat code

--- resolve_and_eco/make_eco_resb_groupcatalog.py
+++ resolve_and_eco/make_eco_resb_groupcatalog.py
@@ -8,12 +8,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 if __name__=='__main__':
+    ########################
+    # Group Finding: ECO
+    ########################
     eco = pd.read_csv('ECOdata_080822.csv')
-    eco1733 = eco[eco.absrmag<-17.33]
-    ecog3grpid, _, meansep, rp1, _, vp1, _, rp2, _, vp2, _  = g3gf(eco.radeg,eco.dedeg,eco.cz,eco.absrmag,-19.4,volume=192351.,rproj_fit_multiplier=3.5,vproj_fit_multiplier=6.5,\
-                    gd_rproj_fit_multiplier=1., gd_vproj_fit_multiplier=2.5, gd_fit_bins=np.arange(-24,-19,0.25), gd_rproj_fit_guess=[1e-5, 0.4], gd_vproj_fit_guess=[3e-5,4e-1])
-    print(meansep)
-    print(rp1)
-    print(vp1)
-    print(rp2)
-    print(vp2)
+    ecogroupsel = (eco.absrmag<-17.33)
+    ecog3grpid = np.zeros(len(eco))-99.
+    _tmp = g3gf(eco[ecogroupsel].radeg,eco[ecogroupsel].dedeg,eco[ecogroupsel].cz,eco[ecogroupsel].absrmag,-19.4,volume=192351.,rproj_fit_multiplier=3.5,vproj_fit_multiplier=6.5,\
+            gd_rproj_fit_multiplier=1., gd_vproj_fit_multiplier=2.5, gd_fit_bins=np.arange(-24,-19,0.25), gd_rproj_fit_guess=[1e-5, 0.4], gd_vproj_fit_guess=[3e-5,4e-1])[0]
+    ecog3grpid[ecogroupsel] = _tmp
+    eco.loc[:,'g3grp_l'] = ecog3grpid
+    eco.to_csv("ECOdata_G3catalog_luminosity.csv",index=False)
+
+    ##########################
+    # Group Finding: RESOLVE
+    ##########################
+    resolve = pd.read_csv('RESOLVEdata_080822.csv')
+    resolve = resolve.set_index('name')
+
+    # get RESOLVE-A from ECO
+    resa_in_eco = eco[eco.resname!='notinresolve'].sort_values(by='resname').set_index('resname')['g3grp_l']
+    resolve = pd.concat([resolve,resa_in_eco])
+    print(resolve.g3grp_l)
